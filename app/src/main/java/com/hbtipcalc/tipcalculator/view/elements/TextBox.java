815cb7389e4c0c860a4d79b106d0ac638589473c
Hunter Baker <bakerhb@dukes.jmu.edu>
2025-10-07 13:07:28 -0400

    textbox start

---
+++ app/src/main/java/com/hbtipcalc/tipcalculator/view/elements/TextBox.java
@@ -0,0 +1,16 @@
+package com.hbtipcalc.tipcalculator.view.elements;
+
+public class TextBox
+{
+    // TODO: this should basically be a label that I could
+    // edit with some commands
+        // backspace
+        // append
+        // clear
+        // setText
+
+    // this needs to have a statically placed decimal because its going to have money
+    // be inserted into it
+    //
+
+}
